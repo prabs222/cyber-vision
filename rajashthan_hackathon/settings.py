e39d358df9f7a95573555258636652b61952267b
Prabhakar Mishra <prabhakarmishra222@gmail.com>
2024-01-02 05:25:00 +0530

    initial5

--- rajashthan_hackathon/settings.py
+++ rajashthan_hackathon/settings.py
@@ -162,10 +162,22 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
+# STATICFILES_DIRS = [ BASE_DIR / 'static']
+# STATIC_ROOT =  BASE_DIR / 'static/'
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 's')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 's')
+
 STATIC_URL = 'static/'
-
-STATIC_ROOT =  BASE_DIR / "s/"
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
