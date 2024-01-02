9695703b8e29016d1c441720cc9e55db4a65488d
Prabhakar Mishra <prabhakarmishra222@gmail.com>
2024-01-02 15:47:28 +0530

    nowpl

--- rajashthan_hackathon/settings.py
+++ rajashthan_hackathon/settings.py
@@ -175,9 +175,9 @@ if DEBUG:
         BASE_DIR / 'static/'
     ]
 else:
-    STATIC_ROOT = BASE_DIR / 'static/'
+    STATIC_ROOT = BASE_DIR / 'static'
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
